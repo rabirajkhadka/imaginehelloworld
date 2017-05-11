400ac13aa3999813df4bd3c0642cbb1fb6ba689e
Rabi Raj Khadka <rabirajkhadka@users.noreply.github.com>
2017-05-11 18:26:18 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPEhtml>
+<html>
+<head>
+<title>Hello Cloud Social Dashboard!</title>
+
+<blockquote class="twitter-tweet" data-lang="en">
+
+<p lang="en" dir="ltr">I just entered for a chance to win $1000 with
+<a href="https://twitter.com/MSFTImagine">@MSFTImagine</a>
+<a href="https://twitter.com/hashtag/HelloCloudSweepstakes?src=hash">#HelloCloudSweepstakes</a> by leveraging the power of 
+<a href="https://twitter.com/hashtag/Azure?src=hash">#Azure</a>! 
+<a href="https://t.co/m1c6jEtQAh">https://t.co/m1c6jEtQAh</a>
+</p>&mdash; rabiraj khadka (@errabirajkhadka) 
+<a href="https://twitter.com/errabirajkhadka/status/862645821736538114">May 11, 2017</a>
+</blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8">
+</script>
+</body>
+</html>
